da756149e58971261aeee856356829ca31be48e5
Dario Lombardo <lomato@gmail.com>
2016-11-23 22:17:25 +0100

    Remove old remotes and force new ones

--- git-util.rb
+++ git-util.rb
@@ -4,11 +4,15 @@ def setup_working_dir(project, logger)
 	if Dir.exists?("#{project['path']}/#{project['name']}")
 		logger.info("Project #{project['name']} already cloned")
 		g = Git.open("#{project['path']}/#{project['name']}", :log => logger)
-		return g
+		g.remotes.each do |remote|
+			g.remote(remote.name).remove
+		end
+		g.add_remote('origin', project['source'])
+	else
+		logger.info("Cloning #{project['name']} into #{project['path']}/#{project['name']}")
+		g = Git.clone(project['source'], project['name'], :path => project['path'],
+			:log => logger)
 	end
-	logger.info("Cloning #{project['name']} into #{project['path']}/#{project['name']}")
-	g = Git.clone(project['source'], project['name'], :path => project['path'],
-		:log => logger)
 	project['destinations'].each do |destination|
 		g.add_remote(destination['name'], destination['url'])
 	end
@@ -16,10 +20,12 @@ def setup_working_dir(project, logger)
 end
 
 def pull_push(g, logger)
+	logger.info("Downloading changes from #{g.remote('origin').name}")
 	g.pull
 	g.remotes.each do |remote|
 		# skip origin
 		next if remote.name == 'origin'
+		logger.info("Pushing changes to #{remote.name}")
 		g.push(remote)
 	end
 end
